19b4376c74b82d5689ae4128780dac510f4006e9
Fadli Hidayatullah <fadlihdytullah@gmail.com>
2023-05-03 15:51:12 +0700

    hotfix: style
    Signed-off-by: Fadli Hidayatullah <fadlihdytullah@gmail.com>

--- src/screen/Dua.jsx
+++ src/screen/Dua.jsx
@@ -18,7 +18,7 @@ function Dua() {
   return (
     <Section id="dua" style={sectionStyle}>
       <div data-aos="zoom-in" className="space-y-4">
-        <p className="text-xl">
+        <p className="text-lg">
           Merupakan suatu kehormatan dan kebahagiaan bagi kami. apabila bapak/ibu/saudara(i)
           berkenan hadir untuk memberikan do'a restu kepada kedua mempelai, Atas kehadiran dan doa
           restu nya kami ucapkan terimakasih
