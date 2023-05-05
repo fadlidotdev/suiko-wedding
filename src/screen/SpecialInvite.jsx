476005621e297522f7d153d803c61658795fba98
Fadli Hidayatullah <fadlihdytullah@gmail.com>
2023-05-05 14:39:20 +0700

    fix font size
    Signed-off-by: Fadli Hidayatullah <fadlihdytullah@gmail.com>

--- src/screen/SpecialInvite.jsx
+++ src/screen/SpecialInvite.jsx
@@ -22,22 +22,22 @@ function SpecialInvite() {
       headerComponent={<Heading>Turut Mengundang</Heading>}
     >
       <div data-aos="zoom-in" className="space-y-4 mb-8">
-        <p className="text-base">
+        <p className="text-lg">
           Keluarga Besar mempelai Wanita <br />
           Keluarga Besar Mempelai Pria
         </p>
 
-        <p className="text-base">
+        <p className="text-lg">
           Atas kehadiran dan do'a restu dari Bapak/Ibu/Saudara/i sekalian, kami mengucapkan Terima
           Kasih.
         </p>
 
-        <p className="text-base">Wassalamu'alaikum Wr.Wb.</p>
+        <p className="text-lg">Wassalamu'alaikum Wr.Wb.</p>
 
         {/* <p className="text-lg font-bold">Meilynda & Rayhan</p> */}
       </div>
 
-      <h2 className="text-xl font-bold drop-shadow-xl font-heading" data-aos="zoom-in">
+      <h2 className="text-3xl font-bold drop-shadow-xl font-heading" data-aos="zoom-in">
         Meilynda &amp; Rayhan
       </h2>
     </Section>
